d1df99b60cae8057c04300dfd68746914d561161
FACT0R1AL <factorial4861@gmail.com>
2025-09-10 11:19:23 +0900

    baekjoon

---
+++ cote/src/cote/exam18108/Main.java
@@ -0,0 +1,18 @@
+package cote.exam18108;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int year = Integer.parseInt(scanner.nextLine());
+		
+		System.out.println(year-543);
+		
+		scanner.close();
+
+	}
+
+}
